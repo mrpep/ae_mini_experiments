93bc6fb29af0ee32dff7aa46ca9b1a1a722f2f78
mrpep <leonardodpepino@gmail.com>
2021-02-09 03:41:14 +0000

    new model using more overlap and pghi

--- ae_trainer.py
+++ ae_trainer.py
@@ -14,7 +14,11 @@ from layers import *
 from models import *
 from losses import *
 
+import librispeech
+
 import dienen
+
+from IPython import embed
 
 gpu_config = {'device': 'auto', 'allow_growth': True}
 gpu_device = 0
@@ -35,9 +39,9 @@ for gpu in gpus:
         print(e)
 logical_gpus = tf.config.experimental.list_logical_devices('GPU')
 
-batch_size = 24
-
-dienen_model = dienen.Model('models/vqvae_wav2vec_speech.yaml')
+batch_size = 16
+audio_train_gen = librispeech.LSGenerator('../LibriSpeech',batch_size=batch_size,winsize=8385)
+dienen_model = dienen.Model('models/cnn_vqvae_speech_stftin.yaml')
 model = dienen_model.build()
 model_config = dienen_model.original_config
 #model = ae_1(16896)
@@ -46,8 +50,8 @@ model.summary()
 def mean_loss(y_true,y_pred):
   return tf.reduce_mean(y_pred)
 
-model.compile(optimizer=tf.keras.optimizers.RMSprop(clipnorm=1.0),loss=SpectralLoss())
-
+#model.compile(optimizer=tf.keras.optimizers.RMSprop(clipnorm=1.0),loss=SpectralLoss())
+model.compile(optimizer=tf.keras.optimizers.RMSprop(clipnorm=1.0),loss=mean_loss)
 #import gsc
 #audio_train_data, audio_test_data = gsc.get_gsc()
 
@@ -58,21 +62,25 @@ model.compile(optimizer=tf.keras.optimizers.RMSprop(clipnorm=1.0),loss=SpectralL
 #import nsynth
 #audio_train_data, audio_test_data = nsynth.read_nsynth('../nsynth')
 
-import librispeech
-audio_train_data, audio_test_data = librispeech.read_librispeech('../LibriSpeech')
+#import librispeech
+#audio_train_data, audio_test_data = librispeech.read_librispeech('../LibriSpeech')
+
+
+audio_train_data, audio_test_data = librispeech.read_librispeech('../LibriSpeech',winsize=8385,n_train=0,n_test=20,calculate_spectrogram=True,frame_size=256,hop_size=64)
 
 from callbacks import WANDBLogger
 
 loggers = {'Spectrograms': {'test_data': [audio_test_data,audio_test_data],
                             'in_layers': ['input_signal'],
-                            'out_layers': ['input_signal', 'estimated_audio'],
-                            'freq': 1,
-                            'unit': 'epoch',
-                            'is_audio': True},
+                            'out_layers': ['original_spectrogram', 'estimated_spectrogram'],
+                            'freq': 2000,
+                            'unit': 'step',
+                            'is_audio': False},
            'TrainMetrics': {'freq': 1, 'unit': 'step'}
           }
 
-wandb.init(name='vqvae_librispeech_raw', project='vqvae_librispeech_raw',config=model.get_config())
-cbks = [WANDBLogger(loggers=loggers),tf.keras.callbacks.ModelCheckpoint('../ckpts/weights.{epoch:02d}-{loss:.2f}.hdf5')]
+wandb.init(name='vqvae_librispeech_pghi', project='vqvae_librispeech_pghi',config=model.get_config())
+cbks = [WANDBLogger(loggers=loggers),tf.keras.callbacks.ModelCheckpoint('../ckpts/weights.{epoch:02d}-{loss:.2f}.hdf5',save_freq=6000)]
 
-model.fit(audio_train_data,audio_train_data,epochs=50,batch_size=batch_size,callbacks = cbks)
+#model.fit(audio_train_data,audio_train_data,epochs=50,batch_size=batch_size,callbacks = cbks)
+model.fit(audio_train_gen,epochs=50,callbacks = cbks)
--- librispeech.py
+++ librispeech.py
@@ -4,15 +4,66 @@ from pathlib import Path
 import numpy as np
 import tqdm
 import soundfile as sf
+from tensorflow.keras.utils import Sequence
+from IPython import embed
+from dsp import stft, get_default_window
 
-def read_librispeech(path, winsize=16000):
+def read_librispeech(path, winsize=16000, n_train=10000, n_test=20, calculate_spectrogram = False, frame_size = None,hop_size=None, window=None):
     files = list(Path(path).rglob('*.flac'))
     wavs = []
-    for f in tqdm.tqdm(files[:10000]):
+    if window is None:
+        window = get_default_window(frame_size)[0]
+    for f in tqdm.tqdm(files[:n_train + n_test]):
         finfo = sf.info(f)
         n_frames = finfo.frames
         wav_i = [sf.read(f,start=i,stop=i+winsize)[0] for i in range(0,n_frames-winsize,winsize)]
+        if calculate_spectrogram:
+            wav_i = [np.abs(stft(x_i,frame_size,hop_size,window)) for x_i in wav_i]
         wavs.extend(wav_i)
     wavs = np.array(wavs)
 
-    return wavs[:-20], wavs[-20:]
+    return wavs[:n_train], wavs[-n_test:]
+
+class LSGenerator(Sequence):
+    def __init__(self,path, batch_size=16,winsize=16000,frame_size=256,hop_size=64):
+        files = list(Path(path).rglob('*.flac'))
+        dfs = []
+        for f in tqdm.tqdm(files):
+            finfo = sf.info(f)
+            n_frames = finfo.frames
+            starts = np.arange(0,n_frames-winsize,winsize)
+            ends = starts + winsize
+            filename = [str(f)]*len(starts)
+            logids = [f.stem+'_{}'.format(i) for i in range(len(starts))]
+            dfs.append(pd.DataFrame({'start': starts, 'end': ends, 'filename': filename, 'logid': logids}).set_index('logid'))
+        self.data = pd.concat(dfs)
+        self.winsize = winsize
+        self.index = np.array(self.data.index)
+        self.batch_size = batch_size
+        self.frame_size = frame_size
+        self.hop_size = hop_size
+        self.window = get_default_window(self.frame_size)[0]
+
+    def read_wav(self,x):
+        try:
+            return sf.read(x['filename'],start=x['start'],stop=x['end'])[0]
+        except:
+            return np.zeros((self.winsize,))
+
+    def __getitem__(self,i):
+        batch_idxs = np.take(self.index,np.arange(i*self.batch_size,(i+1)*self.batch_size),mode='wrap')
+        batch_df = self.data.loc[batch_idxs]
+        #batch_x = batch_df.apply(lambda x: sf.read(x['filename'],start=x['start'],stop=x['end'])[0],axis=1)
+        batch_x = batch_df.apply(lambda x: self.read_wav(x),axis=1)
+        batch_x = np.stack(batch_x)
+        batch_x = np.array([stft(x_i,self.frame_size,self.hop_size,window=self.window) for x_i in batch_x])
+        batch_x = np.abs(batch_x)
+
+        return batch_x, batch_x
+
+    def on_epoch_end(self):
+        self.index = np.random.permutation(self.index)
+
+    def __len__(self):
+        return len(self.data)//self.batch_size
+
